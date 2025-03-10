cc661de9a7305e2e51348bd986109cd101c782e2
denis <denisyakubov321@gmail.com>
2025-03-10 13:09:03 +0300

    more bug fixes v3

--- src/Bottom.js
+++ src/Bottom.js
@@ -2,12 +2,12 @@ import "./Bottom.css"
 
 export default function Bottom(){
     const redirectToForm = () => {
-        window.location.href='';
+        window.location.href='https://docs.google.com/forms/d/e/1FAIpQLSeMPLKhSccI8qUc3zx9UKGDW72Jcvx9aW2AfMTGV2a_CzsOuw/viewform';
     }
     return (<div className="Bottom">
         <button onClick={redirectToForm}>Играть</button>
         <div className="Bottom-Info">
-            <img src="" alt="Фото планеты"/>
+            {/*<img src="" alt="Фото планеты"/>*/}
             <span>©2024-2025 5ive, Contact us</span>
             <span>Legal info | Privacy policy</span>
             <div className="Bottom-Info-Cards">
